fec1ec37a19723903e11a4668b7cb7d51b904a73
Your Name <your_email@example.com>
2026-06-04 03:52:34 -0300

    añadido includes

---
+++ includes/database.php
@@ -0,0 +1,14 @@
+<?php
+$db = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'tienda_hardware'
+);
+
+if(!$db) {
+    echo "Error: No se pudo conectar a MySQL. " . mysqli_connect_error();
+    exit;
+}
+
+mysqli_set_charset($db, 'utf8mb4');
